14497f5fee6818c23b8a542b5d7fcb5cdce9257e
panutrytobeprogrammer <s.panuwatb@gmail.com>
2022-02-13 23:49:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,29 +21,29 @@ client.on('ready', ()=>{
 })
 
 client.on('messageCreate', msg=>{
-    if (msg.content == 'ชนิน'){
+    if (msg.content == 'giav'){
         msg.reply('Lecture on Monday : https://chula.zoom.us/j/99633531372?pwd=b0Jkd05nVXV3MHdlTHFEWU5iVGF0dz09#success\nLecture on Thursday : https://chula.zoom.us/j/96167012836?pwd=Wi9hQUI3eTVFMTN6SE14dVRJc0ZPZz09\nLab on Thursday : https://chula.zoom.us/j/98553123452?pwd=UFFMV1NpZzRpUU43SXRWV1h0NDVjdz09')
     }
-    if (msg.content == 'กรวิก'){
+    if (msg.content == 'math carto'){
         msg.reply('ไปกดในลิ้งในนี้ : https://www.mycourseville.com/?q=courseville/course/27306/meeting')
     }
-    if (msg.content == 'เฉลิมชล'){
+    if (msg.content == 'sat survey'){
         msg.reply('ไปดูใน mycourseville เอาเองนะน้องนะ')
     }
-    if (msg.content == 'ไพศาล'){
+    if (msg.content == 'adv photo'){
         msg.reply('ลิ้งนี้ไอ่สัส : https://chula.zoom.us/j/99162558489?pwd=MEREc3dVbVd2TVlSb2xzYld0QklwZz09')
     }
-    if (msg.content == 'พี่เพลิน'){
+    if (msg.content == 'tech'){
         msg.reply('ลิ้งนี้ไอ่สัส : https://chula.zoom.us/j/97390901679?pwd=L29wb21yOVcybGtMSlpvbW5yNGM2QT09') 
     }
-    if (msg.content == 'ชัยโชค'){
+    if (msg.content == 'adv rs'){
         msg.reply('ใน Microsoft Team!!!!')
     }
     if (msg.content == 'zoom'){
         msg.reply('แปป เดะเขียนให้')
     }
     if (msg.content == '!help'){
-        msg.reply('Random generator : type "!random x x x x" (x are sth you want to random) \nZoom meeting link : พิมพ์ zoom หรือพิมพ์ชื่ออาจารย์เลย')
+        msg.reply('Random generator : type "!random x x x x" (x are sth you want to random) \nZoom meeting link : พิมพ์ zoom หรือพิมพ์ชื่อวิชา \ngiav, math carto, sat survey, tech, adv rs, adv photo')
     }
     if (msg.content[0] == '!'){
         let msgar = msg.content.split(" ")
